018bcc9fa93fd65ba573a319a88c292b59cf1b17
Rohit Gupta <rohitgupta0810@gmail.com>
2023-12-13 14:07:16 +0530

    code

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,14 @@ package com.driver;
 
 public class RWOnly {
     
+    private int var;
+
+    public int getVar(){
+        return var;
+    }
+
+    public void setvar(int val){
+        var = val;
+    }
+ 
 }
