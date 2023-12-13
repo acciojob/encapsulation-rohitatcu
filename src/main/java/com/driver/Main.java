7035b90a4a4d938fc31593deb085375f26f81953
Rohit Gupta <rohitgupta0810@gmail.com>
2023-12-13 14:14:41 +0530

    code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,6 @@ package com.driver;
 public class Main {
    public static void main(String[]args){
     RWOnly obj = new RWOnly();
-    int var = obj.getVar();
+    int var = obj.getName();
    }
 }
